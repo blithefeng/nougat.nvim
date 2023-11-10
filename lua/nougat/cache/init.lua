507dd2deaaceaea223f9f107268512902feb274f
Alexis Kalabura <55039048+axdank@users.noreply.github.com>
2023-11-10 18:48:15 -0300

    feat(cache): support tab cache

--- lua/nougat/cache/init.lua
+++ lua/nougat/cache/init.lua
@@ -2,9 +2,9 @@ local on_event = require("nougat.util").on_event
 
 local mod = {}
 
-local registry = { buf = {}, win = {} }
+local registry = { buf = {}, win = {}, tab = {} }
 
----@param cache_type 'buf'|'win'
+---@param cache_type 'buf'|'win'|'tab'
 ---@param name string
 ---@param default_value? table
 function mod.create_store(cache_type, name, default_value)
@@ -33,7 +33,7 @@ function mod.create_store(cache_type, name, default_value)
   return storage
 end
 
----@param type 'buf'|'win'
+---@param type 'buf'|'win'|'tab'
 ---@param name string
 ---@param id integer
 ---@return any
@@ -41,7 +41,7 @@ function mod.get(type, name, id)
   return registry[type][name][id]
 end
 
----@param type 'buf'|'win'
+---@param type 'buf'|'win'|'tab'
 ---@param id integer
 local function clear_cache(type, id)
   for _, storage in pairs(registry[type]) do
@@ -63,6 +63,23 @@ on_event("WinClosed", function(info)
       clear_cache("win", winid)
     end)
   end
+end)
+
+on_event("TabClosed", function()
+  vim.schedule(function()
+    local active_tabid = {}
+    for _, tabid in ipairs(vim.api.nvim_list_tabpages()) do
+      active_tabid[tabid] = true
+    end
+
+    for _, storage in pairs(registry.tab) do
+      for tabid in pairs(storage) do
+        if not active_tabid[tabid] then
+          storage[tabid] = nil
+        end
+      end
+    end
+  end)
 end)
 
 return mod
