06c370db771c4e9f9ce67a1c921a3f13f493dda7
Munif Tanjim <hello@muniftanjim.dev>
2023-11-25 02:58:54 +0600

    feat: update command :Nougat

--- plugin/nougat.lua
+++ plugin/nougat.lua
@@ -1,54 +1,171 @@
-local completion_store = {
+local cmd_completion_store = {
   [""] = { "profile" },
-  profile = { "bench", "start", "result", "stop" },
+  profile = { "bench", "start", "stop", "inspect" },
+  ["profile:inspect"] = {
+    "bar",
+    "item",
+  },
 }
 
-vim.api.nvim_create_user_command("Nougat", function(opts)
-  local params = vim.split(opts.args, "%s+", { trimempty = true })
+local commands
+commands = {
+  profile = {
+    bench = function()
+      require("nougat.profiler").bench()
+    end,
+    start = function()
+      require("nougat.profiler").start()
+    end,
+    stop = function()
+      require("nougat.profiler").stop()
+    end,
+    inspect = function(params)
+      require("nougat.profiler").inspect(unpack(params.args))
+    end,
+  },
+}
 
-  local mod_name, action_name = params[1], params[2]
-  if not mod_name then
-    print("[Nougat] missing params")
-    return
+local function eval_luastring(value)
+  local ok, result = pcall(loadstring("return " .. value, value))
+  return (not ok or result == nil) and value or result
+end
+
+local function make_params(info, args)
+  local params = {
+    bang = info.bang,
+    args = {},
+  }
+
+  local tbl_arg
+  for _, arg in ipairs(args) do
+    if tbl_arg then
+      tbl_arg = tbl_arg .. arg
+      if string.find(tbl_arg, "}$") then
+        table.insert(params.args, eval_luastring(tbl_arg))
+        tbl_arg = nil
+      end
+    elseif string.find(arg, "^{") then
+      tbl_arg = arg
+      if string.find(tbl_arg, "}$") then
+        table.insert(params.args, eval_luastring(tbl_arg))
+        tbl_arg = nil
+      end
+    elseif string.find(arg, "=") and (not params.args[1] or type(params.args[1]) == "table") then
+      if not params.args[1] then
+        params.args[1] = {}
+      end
+
+      local parts = vim.split(arg, "=")
+      local key = table.remove(parts, 1)
+      local value = table.concat(parts, "=")
+      params.args[1][key] = eval_luastring(value)
+    else
+      table.insert(params.args, eval_luastring(arg))
+    end
   end
 
-  if mod_name == "profile" then
-    if not action_name then
-      print("[Nougat] missing params: " .. opts.args)
-      return
-    end
+  return params
+end
 
-    local mod = require("nougat.profiler")
+vim.api.nvim_create_user_command("Nougat", function(info)
+  local args = info.fargs
 
-    if mod[action_name] then
-      mod[action_name]()
-      return
-    end
+  ---@type string|nil
+  local cmd_name = table.remove(args, 1)
+  if not cmd_name then
+    return vim.api.nvim_err_writeln("[Nougat] missing command")
   end
 
-  print("[Nougat] unknow params: " .. opts.args)
+  local cmd = commands[cmd_name]
+  if type(cmd) == "function" then
+    return cmd(make_params(info, args))
+  end
+
+  if type(cmd) ~= "table" then
+    return vim.api.nvim_err_writeln(string.format("[Nougat] unknown command: %s", cmd_name))
+  end
+
+  if cmd[args[1]] then
+    local subcmd_name = table.remove(args, 1)
+    local subcmd = cmd[subcmd_name]
+
+    if type(subcmd) == "function" then
+      return subcmd(make_params(info, args))
+    end
+
+    if type(subcmd) ~= "table" then
+      return vim.api.nvim_err_writeln(string.format("[Nougat] unknown subcommand: %s", subcmd_name))
+    end
+
+    if subcmd[args[1]] then
+      local subsubcmd_name = table.remove(args, 1)
+      local subsubcmd = subcmd[subsubcmd_name]
+
+      if type(subsubcmd) ~= "function" then
+        return vim.api.nvim_err_writeln(
+          string.format("[Neotest] unknown subcommand: %s %s", subcmd_name, subsubcmd_name)
+        )
+      end
+
+      return subsubcmd(make_params(info, args))
+    end
+
+    if not subcmd[1] then
+      return vim.api.nvim_err_writeln("[Nougat] missing subcommand")
+    end
+
+    return subcmd[1](make_params(info, args))
+  end
+
+  if not cmd[1] then
+    return vim.api.nvim_err_writeln("[Nougat] missing subcommand")
+  end
+
+  return cmd[1](make_params(info, args))
 end, {
   bang = true,
-  nargs = "?",
+  nargs = "*",
   complete = function(_, cmd_line)
-    local has_space = string.match(cmd_line, "%s$")
-    local params = vim.split(cmd_line, "%s+", { trimempty = true })
+    local args = vim.split(cmd_line, "%s+", { trimempty = true })
+    local last_idx = #args
+    local last_arg = args[last_idx]
 
-    if #params == 1 then
-      return completion_store[""]
-    elseif #params == 2 and not has_space then
+    local is_partial = not string.match(cmd_line, "%s$")
+
+    local cmd_scope = ""
+
+    -- command
+    if last_idx == 1 then
+      return cmd_completion_store[cmd_scope]
+    elseif last_idx == 2 and is_partial then
       return vim.tbl_filter(function(cmd)
-        return not not string.find(cmd, "^" .. params[2])
-      end, completion_store[""])
+        return not not string.find(cmd, "^" .. last_arg)
+      end, cmd_completion_store[cmd_scope])
     end
 
-    if #params >= 2 and completion_store[params[2]] then
-      if #params == 2 then
-        return completion_store[params[2]]
-      elseif #params == 3 and not has_space then
-        return vim.tbl_filter(function(cmd)
-          return not not string.find(cmd, "^" .. params[3])
-        end, completion_store[params[2]])
+    -- sub-command
+    cmd_scope = args[2]
+    if last_idx == 2 and cmd_completion_store[cmd_scope] then
+      return cmd_completion_store[cmd_scope]
+    elseif #args == 3 and is_partial and cmd_completion_store[cmd_scope] then
+      local items = vim.tbl_filter(function(cmd)
+        return not not string.find(cmd, "^" .. last_arg)
+      end, cmd_completion_store[cmd_scope])
+      if #items > 0 then
+        return
+      end
+    end
+
+    -- sub-sub-command
+    cmd_scope = string.format("%s:%s", args[2], args[3])
+    if last_idx == 3 and cmd_completion_store[cmd_scope] then
+      return cmd_completion_store[cmd_scope]
+    elseif #args == 4 and is_partial and cmd_completion_store[cmd_scope] then
+      local items = vim.tbl_filter(function(cmd)
+        return not not string.find(cmd, "^" .. last_arg)
+      end, cmd_completion_store[cmd_scope])
+      if #items > 0 then
+        return
       end
     end
   end,
