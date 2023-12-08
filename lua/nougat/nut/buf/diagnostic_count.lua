1f0f65aebbf7490fceef7dde75f00f75cdb9629f
Munif Tanjim <hello@muniftanjim.dev>
2023-12-08 15:11:10 +0600

    fix(nut): buf.diagnostic_count flicker

--- lua/nougat/nut/buf/diagnostic_count.lua
+++ lua/nougat/nut/buf/diagnostic_count.lua
@@ -1,29 +1,63 @@
 local Item = require("nougat.item")
 local core = require("nougat.core")
+local diagnostic_cache = require("nougat.cache.diagnostic")
 local get_hl_def = require("nougat.util.hl").get_hl_def
 local get_hl_name = require("nougat.util.hl").get_hl_name
 local color = require("nougat.color").get()
 
-local diagnostic_cache = require("nougat.cache.diagnostic")
+--luacheck: push no max line length
+
+---@class nougat.nut.buf.diagnostic_count_config.config__severity
+---@field severity integer
+
+---@class nougat.nut.buf.diagnostic_count_config.config__combined
+---@field error? { prefix?: string, suffix?: string, fg?: string }
+---@field warn? { prefix?: string, suffix?: string, fg?: string }
+---@field info? { prefix?: string, suffix?: string, fg?: string }
+---@field hint? { prefix?: string, suffix?: string, fg?: string }
+---@field sep? string
+
+---@class nougat.nut.buf.diagnostic_count_config__severity: nougat_item_config__function
+---@field cache? nil
+---@field config nougat.nut.buf.diagnostic_count_config.config__severity
+---@field content? nil
+---@field hl? nougat_hl_def
+
+---@class nougat.nut.buf.diagnostic_count_config__combined: nougat_item_config__function
+---@field cache? nil
+---@field config? nougat.nut.buf.diagnostic_count_config.config__combined|nougat.nut.buf.diagnostic_count_config.config__combined[]
+---@field content? nil
+---@field hl? nougat_hl_def
+
+---@alias nougat.nut.buf.diagnostic_count_config nougat.nut.buf.diagnostic_count_config__severity|nougat.nut.buf.diagnostic_count_config__combined
+
+--luacheck: pop
+
 local severity = diagnostic_cache.severity
 
 local function calculate_diagnostic_combined_content(cache)
   -- previous combined content
-  cache.pcc = cache.cc
+  cache.pcc = cache.cc or cache.pcc
   -- invalidate combined content
   cache.cc = nil
 end
 
+---@param item NougatItem
+---@param ctx nougat_bar_ctx
 local function default_hidden(item, ctx)
   return item:cache(ctx)[item:config(ctx).severity] == 0
 end
 
+---@param item NougatItem
+---@param ctx nougat_bar_ctx
 local function get_count_content(item, ctx)
   local config = item:config(ctx)
   local count = item:cache(ctx)[config.severity]
   return count > 0 and tostring(count) or ""
 end
 
+---@param item NougatItem
+---@param ctx nougat_bar_ctx
 local function get_combined_content(item, ctx)
   local cache = item:cache(ctx)
 
@@ -44,6 +78,7 @@ local function get_combined_content(item, ctx)
 
     local ctx_hl = { bg = ctx_hl_bg, fg = ctx_hl_fg }
     local item_hl = item.hl or ctx_hl
+    ---@cast item_hl nougat_hl_def
     local sep_hl = config.sep and core.highlight(get_hl_name(item_hl, ctx_hl))
 
     if config.error and cache[severity.ERROR] > 0 then
@@ -99,7 +134,6 @@ local function get_combined_content(item, ctx)
     end
 
     cache.cc = table.concat(parts, nil, 1, part_idx)
-    cache.pcc = nil
   end)
 
   return cache.cc
@@ -107,6 +141,8 @@ end
 
 local hidden = {}
 
+---@param item NougatItem
+---@param ctx nougat_bar_ctx
 local function hidden_if_zero(item, ctx)
   return item:cache(ctx)[item:config(ctx).severity] == 0
 end
@@ -119,42 +155,45 @@ local mod = {
   hidden = hidden,
 }
 
-function mod.create(opts)
+---@param config nougat.nut.buf.diagnostic_count_config
+function mod.create(config)
   diagnostic_cache.enable()
 
-  local config
-  if opts.config and opts.config.severity then
-    config = { severity = opts.config.severity }
+  local resolved_config
+  if config.config and config.config.severity then
+    ---@cast config nougat.nut.buf.diagnostic_count_config__severity
+    resolved_config = { severity = config.config.severity }
   else
     diagnostic_cache.on("change", calculate_diagnostic_combined_content)
 
-    config = vim.tbl_deep_extend("force", {
+    ---@cast config nougat.nut.buf.diagnostic_count_config__combined
+    resolved_config = vim.tbl_deep_extend("force", {
       error = { prefix = "E:", suffix = "", fg = get_hl_def("DiagnosticError").fg or color.red },
       warn = { prefix = "W:", suffix = "", fg = get_hl_def("DiagnosticWarn").fg or color.yellow },
       info = { prefix = "I:", suffix = "", fg = get_hl_def("DiagnosticInfo").fg or color.blue },
       hint = { prefix = "H:", suffix = "", fg = get_hl_def("DiagnosticHint").fg or color.cyan },
       sep = " ",
       severity = severity.COMBINED,
-    }, opts.config or {})
-    ---@cast config -nil
+    }, config.config or {})
+    ---@cast resolved_config -nil
 
-    if config.sep and #config.sep == 0 then
-      config.sep = nil
+    if resolved_config.sep and #resolved_config.sep == 0 then
+      resolved_config.sep = nil
     end
   end
 
   local item = Item({
-    priority = opts.priority,
-    hidden = opts.hidden == nil and default_hidden or opts.hidden,
-    hl = opts.hl,
-    sep_left = opts.sep_left,
-    prefix = opts.prefix,
-    content = config.severity == severity.COMBINED and get_combined_content or get_count_content,
-    suffix = opts.suffix,
-    sep_right = opts.sep_right,
-    config = config,
-    on_click = opts.on_click,
-    context = opts.context,
+    priority = config.priority,
+    hidden = config.hidden == nil and default_hidden or config.hidden,
+    hl = config.hl,
+    sep_left = config.sep_left,
+    prefix = config.prefix,
+    content = resolved_config.severity == severity.COMBINED and get_combined_content or get_count_content,
+    suffix = config.suffix,
+    sep_right = config.sep_right,
+    config = resolved_config,
+    on_click = config.on_click,
+    context = config.context,
     cache = {
       get = function(store, ctx)
         return store[ctx.bufnr]
