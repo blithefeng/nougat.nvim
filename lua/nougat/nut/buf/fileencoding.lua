45bee36ef0c46470bff4b623b9f0c4b98005af80
Munif Tanjim <hello@muniftanjim.dev>
2023-12-10 15:18:55 +0600

    test(nut): add tests for buf.fileencoding

--- lua/nougat/nut/buf/fileencoding.lua
+++ lua/nougat/nut/buf/fileencoding.lua
@@ -1,6 +1,20 @@
 local Item = require("nougat.item")
 
-local function get_content(item, ctx)
+--luacheck: push no max line length
+
+---@class nougat.nut.buf.fileencoding_config.config
+---@field text? table<'bomb'|'noendofline', string?>
+
+---@class nougat.nut.buf.fileencoding_config: nougat_item_config__function
+---@field cache? nil
+---@field config? nougat.nut.buf.fileencoding_config.config|nougat.nut.buf.fileencoding_config.config[]
+---@field content? nil
+
+--luacheck: pop
+
+---@param item NougatItem
+---@param ctx nougat_bar_ctx
+local function content(item, ctx)
   local text = item:config(ctx).text
   return table.concat({
     vim.api.nvim_buf_get_option(ctx.bufnr, "fileencoding"),
@@ -11,24 +25,25 @@ end
 
 local mod = {}
 
-function mod.create(opts)
+---@param config nougat.nut.buf.fileencoding_config
+function mod.create(config)
   local item = Item({
-    priority = opts.priority,
-    hidden = opts.hidden,
-    hl = opts.hl,
-    sep_left = opts.sep_left,
-    prefix = opts.prefix,
-    content = get_content,
-    suffix = opts.suffix,
-    sep_right = opts.sep_right,
+    priority = config.priority,
+    hidden = config.hidden,
+    hl = config.hl,
+    sep_left = config.sep_left,
+    prefix = config.prefix,
+    content = content,
+    suffix = config.suffix,
+    sep_right = config.sep_right,
     config = vim.tbl_extend("force", {
       text = {
         bomb = "[BOM]",
         noendofline = "[!EOL]",
       },
-    }, opts.config or {}),
-    on_click = opts.on_click,
-    context = opts.context,
+    }, config.config or {}),
+    on_click = config.on_click,
+    context = config.context,
   })
 
   return item
---
+++ tests/nougat/nut/buf/fileencoding_spec.lua
@@ -0,0 +1,72 @@
+pcall(require, "luacov")
+
+local Bar = require("nougat.bar")
+
+local t = require("tests.util")
+
+describe("nut.buf.fileencoding", function()
+  local bar, ctx
+
+  local nut
+
+  before_each(function()
+    require("nougat.util.store").clear_all()
+
+    bar = Bar("statusline")
+    ctx = t.make_ctx(0, {
+      ctx = {},
+      width = vim.api.nvim_win_get_width(0),
+    })
+
+    nut = require("nougat.nut.buf.fileencoding")
+  end)
+
+  after_each(function()
+    if ctx.bufnr then
+      vim.api.nvim_buf_delete(ctx.bufnr, { force = true })
+    end
+  end)
+
+  it("works w/o config", function()
+    bar:add_item(nut.create({}))
+
+    t.eq(bar:generate(ctx), "")
+
+    vim.bo[ctx.bufnr].fileencoding = "UTF-8"
+    t.eq(bar:generate(ctx), "utf-8")
+
+    vim.bo[ctx.bufnr].bomb = true
+    t.eq(bar:generate(ctx), "utf-8[BOM]")
+
+    vim.bo[ctx.bufnr].endofline = false
+    t.eq(bar:generate(ctx), "utf-8[BOM][!EOL]")
+
+    vim.bo[ctx.bufnr].fileencoding = ""
+    t.eq(bar:generate(ctx), "[BOM][!EOL]")
+  end)
+
+  it("works w/ config", function()
+    bar:add_item(nut.create({
+      config = {
+        text = {
+          bomb = "(B)",
+          noendofline = "(!)",
+        },
+      },
+    }))
+
+    t.eq(bar:generate(ctx), "")
+
+    vim.bo[ctx.bufnr].fileencoding = "UTF-8"
+    t.eq(bar:generate(ctx), "utf-8")
+
+    vim.bo[ctx.bufnr].bomb = true
+    t.eq(bar:generate(ctx), "utf-8(B)")
+
+    vim.bo[ctx.bufnr].endofline = false
+    t.eq(bar:generate(ctx), "utf-8(B)(!)")
+
+    vim.bo[ctx.bufnr].fileencoding = ""
+    t.eq(bar:generate(ctx), "(B)(!)")
+  end)
+end)
