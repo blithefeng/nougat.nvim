2de6572bf551084e5f8bd5994df2e459a39c8fcd
linrongbin16 <linrongbin16@outlook.com>
2024-01-04 15:10:02 +0800

    test(nut): add 'vim-gitgutter' provider tests for git.status

--- tests/nougat/nut/git/status_spec.lua
+++ tests/nougat/nut/git/status_spec.lua
@@ -10,6 +10,8 @@ describe("nut.git.status", function()
   local nut
 
   before_each(function()
+    vim.wait(0)
+
     require("nougat.store")._clear()
 
     bar = Bar("statusline")
@@ -27,56 +29,124 @@ describe("nut.git.status", function()
     end
   end)
 
-  it("works", function()
-    package.loaded["gitsigns"] = {}
+  describe("w/ gitsigns", function()
+    it("works", function()
+      package.loaded["gitsigns"] = {}
 
-    bar:add_item(nut.create({
-      prefix = " ",
-      content = {
-        nut.count("added", { prefix = "+", suffix = " " }),
-        nut.count("changed", { prefix = "~", suffix = " " }),
-        nut.count("removed", { prefix = "-", suffix = " " }),
-      },
-    }))
+      bar:add_item(nut.create({
+        prefix = " ",
+        content = {
+          nut.count("added", { prefix = "+", suffix = " " }),
+          nut.count("changed", { prefix = "~", suffix = " " }),
+          nut.count("removed", { prefix = "-", suffix = " " }),
+        },
+      }))
 
-    package.loaded["gitsigns"] = nil
+      package.loaded["gitsigns"] = nil
 
-    vim.b[ctx.bufnr].gitsigns_status_dict = {
-      added = 1,
-    }
-    vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
-    vim.wait(0)
+      vim.b[ctx.bufnr].gitsigns_status_dict = {
+        added = 1,
+      }
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
+      vim.wait(0)
 
-    t.eq(bar:generate(ctx), " +1 ")
+      t.eq(bar:generate(ctx), " +1 ")
 
-    vim.b[ctx.bufnr].gitsigns_status_dict = {
-      added = 0,
-      changed = 2,
-      removed = 3,
-    }
-    vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
-    vim.wait(0)
+      vim.b[ctx.bufnr].gitsigns_status_dict = {
+        added = 0,
+        changed = 2,
+        removed = 3,
+      }
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
+      vim.wait(0)
 
-    t.eq(bar:generate(ctx), " ~2 -3 ")
+      t.eq(bar:generate(ctx), " ~2 -3 ")
+    end)
+
+    it("can handle missing gitstatus", function()
+      package.loaded["gitsigns"] = {}
+
+      bar:add_item(nut.create({
+        prefix = " ",
+        content = {
+          nut.count("added", { prefix = "+", suffix = " " }),
+        },
+      }))
+
+      package.loaded["gitsigns"] = nil
+
+      vim.b[ctx.bufnr].gitsigns_status_dict = nil
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
+      vim.wait(0)
+
+      t.eq(bar:generate(ctx), "")
+    end)
   end)
 
-  it("can handle missing gitstatus", function()
-    package.loaded["gitsigns"] = {}
+  describe("w/ vim-gitgutter", function()
+    before_each(function()
+      t.stub(vim.fn, "exists", function(name)
+        return name == "*GitGutterGetHunkSummary" and 1 or 0
+      end)
+    end)
 
-    bar:add_item(nut.create({
-      prefix = " ",
-      content = {
-        nut.count("added", { prefix = "+", suffix = " " }),
-      },
-    }))
+    after_each(function()
+      vim.fn.exists:revert()
 
-    package.loaded["gitsigns"] = nil
+      if type(vim.fn.GitGutterGetHunkSummary) == "table" and vim.fn.GitGutterGetHunkSummary.revert then
+        vim.fn.GitGutterGetHunkSummary:revert()
+      end
+    end)
 
-    vim.b[ctx.bufnr].gitsigns_status_dict = nil
-    vim.api.nvim_exec_autocmds("User", { pattern = "GitSignsUpdate" })
-    vim.wait(0)
+    it("works", function()
+      bar:add_item(nut.create({
+        prefix = " ",
+        content = {
+          nut.count("added", { prefix = "+", suffix = " " }),
+          nut.count("changed", { prefix = "~", suffix = " " }),
+          nut.count("removed", { prefix = "-", suffix = " " }),
+        },
+      }))
 
-    t.eq(bar:generate(ctx), "")
+      t.stub(vim.fn, "GitGutterGetHunkSummary", function()
+        return { 1 }
+      end)
+
+      vim.g.gitgutter_hook_context = { bufnr = ctx.bufnr }
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitGutter" })
+      vim.wait(0)
+
+      t.eq(bar:generate(ctx), " +1 ")
+
+      vim.fn.GitGutterGetHunkSummary:revert()
+
+      t.stub(vim.fn, "GitGutterGetHunkSummary", function()
+        return { 2, 4, 6 }
+      end)
+
+      vim.g.gitgutter_hook_context = { bufnr = ctx.bufnr }
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitGutter" })
+      vim.wait(0)
+
+      t.eq(bar:generate(ctx), " +2 ~4 -6 ")
+    end)
+
+    it("can handle missing gitstatus", function()
+      bar:add_item(nut.create({
+        prefix = " ",
+        content = {
+          nut.count("added", { prefix = "+", suffix = " " }),
+        },
+      }))
+
+      t.stub(vim.fn, "GitGutterGetHunkSummary")
+
+      vim.g.gitgutter_hook_context = { bufnr = ctx.bufnr }
+      vim.api.nvim_exec_autocmds("User", { pattern = "GitGutter" })
+      vim.wait(0)
+
+      t.eq(bar:generate(ctx), "")
+    end)
   end)
 
   it("can handle missing provider", function()
